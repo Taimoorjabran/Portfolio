57bac8da17c35cec3a4d9368de5584b63c3d56d1
Taimoorjabran <taimoorjabran786@gmail.com>
2025-06-19 12:14:33 +0530

    Fixed user name

--- src/models/User.ts
+++ src/models/User.ts
@@ -11,6 +11,7 @@ export interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
+  name: { type: String, required: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   lastChattedWith: String,
